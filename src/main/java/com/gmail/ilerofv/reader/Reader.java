a46eb45b433a6bb0f842c47fd678a760109bafd0
ilerofv <ilerofv@gmail.com>
2020-10-27 21:43:01 +0300

    update maven build and new csv location

--- src/main/java/com/gmail/ilerofv/reader/Reader.java
+++ src/main/java/com/gmail/ilerofv/reader/Reader.java
@@ -6,28 +6,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Reader {
-    ClassLoader classLoader = getClass().getClassLoader();
-
     public List<String[]> read(String filePath) {
-        URL url = classLoader.getResource(filePath);
-        FileReader fileReader = null;
         List<String[]> list = new ArrayList<>();
-        if (url != null) {
-            try {
-                fileReader = new FileReader(new File(url.getFile()));
-            } catch (FileNotFoundException e) {
-                //TODO нормальная ошибка
-                e.printStackTrace();
-                return list;
-            }
-        } else {
-            return list;
+        FileReader fileReader = null;
+        try {
+            File file = new File(filePath);
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            //TODO обработать
+            e.printStackTrace();
+            return null;
         }
         try (CSVReader reader = new CSVReader(fileReader)) {
             //Read CSV line by line and use the string array as you want
