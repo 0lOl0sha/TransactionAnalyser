818055612b73c09d85c70677ae52453a20334078
ilerofv <ilerofv@gmail.com>
2020-10-31 12:22:36 +0300

    deleting a comment

--- src/main/java/com/gmail/ilerofv/entity/Transaction.java
+++ src/main/java/com/gmail/ilerofv/entity/Transaction.java
@@ -23,54 +23,5 @@ public class Transaction {
     private String merchant;
     private TransactionType transactionType;
     private String related = "";
-
-
-
-//    public static class Builder {
-//        private Transaction newTransaction;
-//
-//        public Builder(){
-//            newTransaction = new Transaction();
-//        }
-//
-//        public Builder withId(String id){
-//            newTransaction.id = id;
-//            return this;
-//        }
-//
-//        public Builder withDate(Date date){
-//            newTransaction.date = date;
-//            return this;
-//        }
-//
-//        public Builder withAmount(double amount){
-//            newTransaction.amount = amount;
-//            return this;
-//        }
-//
-//        public Builder withMerchant(String merchant){
-//            newTransaction.merchant = merchant;
-//            return this;
-//        }
-//
-//        public Builder withType(Type type){
-//            newTransaction.type = type;
-//            return this;
-//        }
-//
-//        public Builder withRelated(String related){
-//            newTransaction.related = related;
-//            return this;
-//        }
-//
-//        public Builder reset (){
-//            newTransaction = new Transaction();
-//            return this;
-//        }
-//
-//        public Transaction build() {
-//            return newTransaction;
-//        }
-//    }
-
+    
 }
